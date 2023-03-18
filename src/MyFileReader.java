aec2116dba45baee7e386360e4c3ed3dc51debb8
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-03-17 19:27:47 -0500

    added classes

---
+++ src/MyFileReader.java
@@ -0,0 +1,59 @@
+//import java.io.BufferedReader;
+//import java.io.FileNotFoundException;
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//
+///**
+// * Class to read lines from a file.
+// */
+//public class MyFileReader {
+//
+//    /**
+//     * Name of file being read.
+//     */
+//    private String filename;
+//
+//    /**
+//     * Creates MyFileReader with given filename to read.
+//     * @param filename to read
+//     */
+//    public MyFileReader(String filename) {
+//        this.filename = filename;
+//    }
+//
+//    /**
+//     * Opens the file specified by filename and reads the text line by line.
+//     * Cleans up each line by trimming whitespace from the beginning and end of each line.
+//     * Adds each line to an ArrayList<String> which is returned from the method.
+//     * If a line is empty (does not contain any text), it's skipped and is not added to the ArrayList<String>.
+//     *
+//     * Example:
+//     * - Calling getCleanContent() will open and read the file, clean up the text line by line,
+//     * add each line containing text to an ArrayList<String>, and return the ArrayList<String> from the method.
+//     *   If there is a line "nice to meet you     ", it will become "nice to meet you"
+//     *   If there is a line "    hello world    ", it will become "hello world"
+//     *   If there is a line "today is a nice day", it will remain "today is a nice day"
+//     *   If there is a line " ", it will be skipped and not added to the ArrayList<String>
+//     *
+//     * @return list of lines with no empty spaces at the beginning or end of each line
+//     */
+//    public ArrayList<String> getCleanContent() {
+//        ArrayList<String> cleanLines = new ArrayList<>();
+//        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+//            String line = reader.readLine();
+//            while (line != null) {
+//                String cleanLine = line.trim();
+//                if (cleanLine.length() > 0) {
+//                    cleanLines.add(cleanLine);
+//                }
+//                line = reader.readLine();
+//            }
+//        } catch (FileNotFoundException e) {
+//            System.err.println("File not found: " + filename);
+//        } catch (IOException e) {
+//            System.err.println("Error reading file: " + filename);
+//        }
+//        return cleanLines;
+//    }
+//}
